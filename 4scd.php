46f249774164f53345de39a2f54d27e5cb0f917e
tortex060 <javipl88@gmail.com>
2016-02-07 19:42:02 +0100

    4SCD DONE

--- 4scd.php
+++ 4scd.php
@@ -2,5 +2,75 @@
   include ("navbar.html");
  ?>
  <header>
-
+   <div class="page-title">
+     <h2>#4SCD</h2>
+   </div>
  </header>
+ <script type="text/javascript">
+
+ </script>
+<div class="page-container">
+  <div class="page-content">
+    <div class="scd-image-single displayResp" style="order: 2">
+      <img src="/images/info4scd/4scd.jpg" alt="" />
+    </div>
+    <div class="scd-image-multi displayResp" style="order: 4">
+      <img src="/images/info4scd/organizadore-lavernia.jpg" alt="" style="width: 100%"/>
+      <img src="/images/info4scd/tiscar.jpg" alt="" /><img src="/images/info4scd/jmila.jpg" alt="" />
+      <p class="light-bg image-footer">Nacho Lavernia y miembros de la organización de la #3SCD, Carlos Tiscar (izq) y José María Milá de Santa&Cole (der)</p>
+    </div>
+    <div class="scd-image-single displayResp" style="order: 6">
+      <img src="/images/info4scd/granada.jpg" alt="" />
+      <p class="light-bg image-footer">Taller "Servicios asociados a productos". Granada Barrero</p>
+    </div>
+    <div class="scd-text-column-right displayResp" style="order: 5">
+      <h4>EDICIONES ANTERIORES</h4>
+      <h5> </h5>
+      <p class="light-bg"><b>La primera edición, SCD,</b> surge en octubre de 2011, como una iniciativa de crear algo nuevo. Asistieron alumnos de diversos estudios de la Universidad de Sevilla y se plantearon charlas, cineforums y talleres. Participaron entidades como Lab Sevilla, I Do Proyect, CommonFab, Background 3D, Indico Design y el Grupo Joly, así como a la dirección de la Escuela Politécnica Superior, la Asociación Andaluza de Diseñadores, Diario de Sevilla, HEDA Patentes, Co-working Sevilla, Lugadero y Diseñadero.</p>
+
+      <p class="light-bg"><b>La segunda edición, #2SCD,</b> desarrollada en octubre de 2013, aumentó de forma sorprendente en todos sus aspectos. Este evento duplicó el numero de asistentes, y cobró mayor relevancia. Se desarrolló en el Centro de Iniciativas Culturales de la Universidad de Sevilla (CICUS) y en el Teatro La Fundición. Se contó con la participación, entre otros, de José Manuel Mateo (Diseñador Industrial- Ozestudi), Óscar López (Estudio Reina de Corazones), Mario Castellanos (Profesor de Marketing en la Universidad de Sevilla), Granada Barrero (AAD), Ana María Cortijo (De la clase a la cuenta), Quim Larrea (Surgenia), Xavier Coller (Universidad Pablo de Olavide) y Santiago Cirugeda (Recetas Urbanas).</p>
+    </div>
+    <div class="scd-text-column-left">
+      <div class="scd-image-single displayDesktop">
+        <img src="/images/info4scd/4scd.jpg" alt="" />
+      </div>
+      <h4>ANTECEDENTES. #3SCD</h4>
+      <h5> </h5>
+      <p class="light-bg">La pasada edición del evento, la #3SCD tuvo lugar en Marzo de 2015. Se decidió contactar con los profesionales y empresas más relevantes del sector del diseño nacional.</p>
+
+      <p class="light-bg">El evento fue una maratón que comienzó con Carlos Tíscar invitándonos a pensar en qué momento decidimos ser diseñadores industriales, para pasar a que Enrique Romero y Miguel Ángel Pérez de Racormance nos hablasen del proceso de aprendizaje, de problemas y soluciones. Blanca Drake nos contó cómo nace Vitra y nos vislumbró a algunos clásicos del diseño.</p>
+
+      <p class="light-bg">Laura Molina y Sergio Herrera, de Todomuta, nos enseñaron materiales tratados artesanalmente, mientras que José María Millá y Josep María Serra, de Santa&Cole, nos mostraron todo el proceso industrial para elaborar el banco neorromántico y las distintas versiones del mismo en otros materiales, haciendo hincapié en el valor y la calidad de lo auténtico frente a copias más baratas.</p>
+
+      <p class="light-bg">Óscar Vera, de Vondom, nos explicó la importancia de saber transmitir una idea y Fernando Infante, de El Golpe, nos hizo ver la importancia de lo gráfico en el producto. Marcelo Alegre, nos presentó su estudio y el proceso para realizar una silla de oficina, y Pablo Crespo, de Imaisde, nos mostró diversos trabajos que había realizado, dándonos claves para alcanzar el éxito profesional.</p>
+
+      <p class="light-bg">La ponencia de Nacho Lavernia giraba en torno a la frase “La creatividad es como la mermelada...” mientras que Javier Peña, de Materfad, realizó un análisis de materiales como el grafeno o la nanocelulosa, dejándonos impresionados.</p>
+
+      <p class="light-bg">Se realizaron mesas de debate contando con la presecia de estos profesionales y talleres para todos los gustos durante la mañana del sábado: sobre packaging, metodologías de diseño, modelado y prototipado, emprendimiento y  fabricación aditiva. </p>
+      <div class="scd-image-single displayDesktop">
+        <img src="/images/info4scd/granada.jpg" alt="" />
+        <p class="light-bg image-footer">Taller "Servicios asociados a productos". Granada Barrero</p>
+      </div>
+    </div>
+
+    <div class="scd-text-column-right">
+      <h4>#4SCD INDIVISIBLE KNOWLEDGE</h4>
+      <h5> </h5>
+      <p class="light-bg">La Asociación de Estudiantes de Diseño Industrial se enorgullece de presentar la #4SCD, el mayor congreso de Diseño Industrial desarrollado en Andalucía, que sirve como punto de encuentro para que estudiantes y profesionales del sector interactúen y pongan en común sus conocimientos e impresiones, debatiendo, aprendiendo y difundiendo esta hermosa disciplina.</p>
+
+      <p class="light-bg">Cada año abordamos un enfoque distinto del Diseño Industrial, manteniendo siempre una visión actual e integral de todos los aspectos que lo definen. En esta edición, “Indivisible Knowledge”, partimos de unas palabras del genio Asimov para llegar a la conclusión de que todas las ramas del Diseño son dependientes unas de otras. La Tecnología y la Electrónica serán el pilar fundamental en este acercamiento al Diseño Industrial.</p>
+
+      <p class="light-bg">La #4SCD define un entorno donde intercambiar las múltiples visiones que conforman el sector. Para ello, a través de conferencias, debates, contacto con empresas y múltiples workshops, los ingenieros, diseñadores industriales, arquitectos y otras profesiones vinculadas al Diseño Industrial, aprenderán y compartirán su saber con el fin ulterior de expandir, difundir el conocimiento sobre esta área y mejorar la sociedad que nos rodea.</p>
+      <div class="scd-image-multi displayDesktop">
+        <img src="/images/info4scd/organizadore-lavernia.jpg" alt="" style="width: 100%"/>
+        <img src="/images/info4scd/tiscar.jpg" alt="" /><img src="/images/info4scd/jmila.jpg" alt="" />
+        <p class="light-bg image-footer">Nacho Lavernia y miembros de la organización de la #3SCD, Carlos Tiscar (izq) y José María Milá de Santa&Cole (der)</p>
+      </div>
+      <h4 class="displayDesktop">EDICIONES ANTERIORES</h4>
+      <h5 class="displayDesktop"> </h5>
+      <p class="light-bg displayDesktop"><b>La primera edición, SCD,</b> surge en octubre de 2011, como una iniciativa de crear algo nuevo. Asistieron alumnos de diversos estudios de la Universidad de Sevilla y se plantearon charlas, cineforums y talleres. Participaron entidades como Lab Sevilla, I Do Proyect, CommonFab, Background 3D, Indico Design y el Grupo Joly, así como a la dirección de la Escuela Politécnica Superior, la Asociación Andaluza de Diseñadores, Diario de Sevilla, HEDA Patentes, Co-working Sevilla, Lugadero y Diseñadero.</p>
+
+      <p class="light-bg displayDesktop"><b>La segunda edición, #2SCD,</b> desarrollada en octubre de 2013, aumentó de forma sorprendente en todos sus aspectos. Este evento duplicó el numero de asistentes, y cobró mayor relevancia. Se desarrolló en el Centro de Iniciativas Culturales de la Universidad de Sevilla (CICUS) y en el Teatro La Fundición. Se contó con la participación, entre otros, de José Manuel Mateo (Diseñador Industrial- Ozestudi), Óscar López (Estudio Reina de Corazones), Mario Castellanos (Profesor de Marketing en la Universidad de Sevilla), Granada Barrero (AAD), Ana María Cortijo (De la clase a la cuenta), Quim Larrea (Surgenia), Xavier Coller (Universidad Pablo de Olavide) y Santiago Cirugeda (Recetas Urbanas).</p>
+    </div>
+  </div>
+</div>
